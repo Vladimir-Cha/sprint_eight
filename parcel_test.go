cebef433b55003d012a5525cca9929b288b6b0ec
Vadim Mukhametshin <da-vadik@yandex.ru>
2024-02-20 15:15:01 +0300

    second

--- parcel_test.go
+++ parcel_test.go
@@ -159,7 +159,8 @@ func TestGetByClient(t *testing.T) {
 	// add
 	for i := 0; i < len(parcels); i++ {
 		// добавьте новую посылку в БД, убедитесь в отсутствии ошибки и наличии идентификатора
-		id, _ := store.Add(parcels[i])
+		id, err := store.Add(parcels[i])
+		require.NoError(t, err)
 
 		// обновляем идентификатор добавленной у посылки
 		parcels[i].Number = id
